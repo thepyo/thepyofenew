9d080a78abb563dae1ef85c4604d3c9f3e46387c
tuanpinggo <tuannh@pinggo.vn>
2024-11-22 01:56:38 +0700

    first

---
+++ src/theme/config.js
@@ -0,0 +1,10 @@
+export const globalConfig = {
+    maxWidth: 'xl',
+    api_url: 'https://admin.bacsivutrongtien.vn/api',
+    // api_url: 'http://localhost:1337/api',
+    revalidateTime: 3000,
+    cdn_url: 'https://admin.bacsivutrongtien.vn',
+    // cdn_url: 'http://localhost:1337',
+    home_url: 'https://thepyo.vn',
+    site_name: 'Viện thẩm mỹ The Pyo'
+}
